151a197b9a7ee8a9c8188f5633aa19e70bb19c8e
hanna <muse.minimal@gmail.com>
2020-10-01 14:45:46 +0300

    slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -274,10 +274,16 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     return await res.json();
-  };
+  }; // getResource('http://localhost:3000/menu')
+  //     .then(data => {
+  //         data.forEach(({img, altimg, title, descr, price}) => {
+  //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //         });
+  //     });
 
-  getResource('http://localhost:3000/menu').then(data => {
-    data.forEach(({
+
+  axios.get('http://localhost:3000/menu').then(data => {
+    data.data.forEach(({
       img,
       altimg,
       title,
@@ -374,7 +380,52 @@ window.addEventListener('DOMContentLoaded', () => {
       previousModalDialog.classList.remove('hide');
       closeModal();
     }, 4000);
+  } //Slider
+
+
+  const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
   }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => item.style.display = 'none');
+    slides[slideIndex - 1].style.display = 'block';
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
 });
 
 /***/ })
